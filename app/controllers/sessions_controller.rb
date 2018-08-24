2477c64b76339824b7ec61b6bf5982fa65f37349
Alexander Nakos <Alex.Nakos9@gmail.com>
2018-08-24 23:06:20 +0000

    Complete authentication system

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  
+  def new 
+  
+  end
+  
+  def create 
+    #login
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in!"
+      redirect_to user_path(user)
+      
+    else 
+      flash.now[:danger] = "There was something wrong with you login information."
+      render 'new'
+    end
+  end
+  
+  def destroy
+    #logout
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out!"
+    redirect_to root_path
+  end 
+  
+
+end
